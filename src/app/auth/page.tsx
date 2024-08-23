402a17d6c7542f21f03c4409a4c8a80866db949e
MahdiYosofzay <mahdiyosofzay@gmail.com>
2024-08-23 19:27:42 +0330

    Auth route added and shadcn form component added

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <form action="">
+      <input name="name" />
+      <input name="email" />
+      <input name="password" />
+    </form>
+  );
+};
+
+export default page;
